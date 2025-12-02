d386efb1537c13f0ce88427cc2aec6269313678e
Peixe-blue <ras52@discente.ifpe.edu.br>
2025-12-02 09:30:27 -0300

    Etapa 2 do projeto de Dispositivos Móveis

--- app/src/main/java/com/example/elasegura/model/MainViewModel.kt
+++ app/src/main/java/com/example/elasegura/model/MainViewModel.kt
@@ -2,9 +2,7 @@ package com.example.elasegura.model
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
-import com.google.android.gms.maps.model.LatLng
 
 class MainViewModel : ViewModel() {
     // localização atual simulada
@@ -25,3 +23,4 @@ class MainViewModel : ViewModel() {
         _contacts.add(contact)
     }
 }
+
